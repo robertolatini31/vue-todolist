f0ec0c239115a17af551eef5e804c6b70fed5706
robertolatini31 <robertolatini31@gmail.com>
2022-04-22 13:26:08 +0200

    added css-html structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,31 @@
 const app = new Vue ({
     el: '#app',
     data:{
-        
+        tasks: [
+            {
+                text: 'Fare i compiti',
+                done: false
+            },
+            {
+                text: 'Fare la spesa',
+                done: false
+            },
+            {
+                text: 'Fare il bucato',
+                done: true
+            }
+        ]
     }
 });
 
+// Rifare l'esercizio della to do list. 
+// Questa volta però ogni todo sarà un oggetto, 
+// formato da due proprietà:
+// text, una stringa che indica il testo del todo
+// done, un booleano (true/false) che indica 
+// se il todo è stato fatto oppure no
+
+// MILESTONE 1
+// Stampare all'interno di una lista, 
+// un item per ogni todo. Se la proprietà done 
+// è uguale a true, visualizzare il testo del todo sbarrato.
